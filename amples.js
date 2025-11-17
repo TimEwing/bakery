334f754eade531303757a5c6d60285360dadcbcc
Tim Ewing <TimJEwing@gmail.com>
2025-11-16 18:11:19 -0800

    ex-

---
+++ amples.js
@@ -0,0 +1,7 @@
+
+////// DRUMS //////
+//hh
+s("hh").struct("1([2|3|5], 8)".fast(2)).bank("tr808") // random clicky
+
+// bd
+$: s("bd*4").bank("polaris").room(.1) // THICC AS FUCK four on the floor
