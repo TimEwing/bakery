1543624cdaf25da95e6e572c7fdb77d89c8f8b3f
Tim Ewing <TimJEwing@gmail.com>
2025-11-23 21:19:06 -0800

    catoutput helped

--- amples.js
+++ amples.js
@@ -1,4 +1,20 @@
 
+
+////// NOT DRUMS //////
+
+_soundsLikeATrumpetOrSomething: n("0 5 ~ ~")
+.chord("Cdim".slow(8))
+.anchor("C4,C5")
+.voicing()
+.s("saw")
+.sometimes(ply("2|4"))
+.patt(0.1)
+.pan(1)
+
+.off(3/8, x=>x.lp("3000:5").velocity(.75).pan(0) 
+  .off(3/8, y=>y.lp("1500:5").velocity(.5).pan(1))
+)  // siiiick
+
 ////// DRUMS //////
 //hh
 s("hh").struct("1([2|3|5], 8)".fast(2)).bank("tr808") // random clicky
